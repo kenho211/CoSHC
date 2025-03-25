da631717b685271f655d502846ff0bdd65540fc4
kenho211 <hokinman12055@gmail.com>
2025-03-25 22:53:32 +0800

    fix

--- CodeBERT/run_coshc.py
+++ CodeBERT/run_coshc.py
@@ -16,42 +16,13 @@ import json
 import numpy as np
 from sklearn.cluster import KMeans # type: ignore
 from model import Model as BaseModel, CoSHCModel
+from run import TextDataset
 from torch.nn import CrossEntropyLoss, MSELoss, CosineSimilarity # type: ignore
 from torch.utils.data import DataLoader, Dataset, SequentialSampler # type: ignore
 from transformers import RobertaConfig, RobertaTokenizer, RobertaModel # type: ignore
 from datetime import datetime
 
 logger = logging.getLogger(__name__)
-
-
-class TextDataset(Dataset):
-    def __init__(self, tokenizer, args, file_path=None):
-        self.examples = []
-        data=[]
-        with open(file_path) as f:
-            for line in f:
-                line=line.strip()
-                js=json.loads(line)
-                data.append(js)
-
-        for js in data:
-            self.examples.append(convert_examples_to_features(js,tokenizer,args))
-            
-        if 'train' in file_path:
-            for idx, example in enumerate(self.examples[:3]):
-                logger.info("*** Example ***")
-                logger.info("idx: {}".format(idx))
-                logger.info("code_tokens: {}".format([x.replace('\u0120','_') for x in example.code_tokens]))
-                logger.info("code_ids: {}".format(' '.join(map(str, example.code_ids))))
-                logger.info("nl_tokens: {}".format([x.replace('\u0120','_') for x in example.nl_tokens]))
-                logger.info("nl_ids: {}".format(' '.join(map(str, example.nl_ids))))                             
-        
-    def __len__(self):
-        return len(self.examples)
-
-    def __getitem__(self, i):   
-        return (torch.tensor(self.examples[i].code_ids),torch.tensor(self.examples[i].nl_ids))
-
 
 # --------------------------------------------------
 # CoSHC Training Utilities
